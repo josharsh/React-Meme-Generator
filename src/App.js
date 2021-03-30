8ad14624be29881a784763fd99d1b70c070c0295
josharsh <harsh.joshi.pth@gmail.com>
2021-03-31 01:30:40 +0530

    Meme Project Init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MemeGenerator from './components/MemeGenerator';
+import Header from './components/Header';
+function App() {
+  return (
+    <div>
+    <Header/>
+    <MemeGenerator/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+export default function Header(){
+    return(
+        <header>
+            <img 
+                src="https://media.tenor.com/images/479ffe4fe3a5b98f21802e39f920451a/tenor.gif" 
+                alt="Problem?"
+            />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
